55d1be55504f9330269d6302e75417bdc46613ac
David Liu <david.yx.liu@oracle.com>
2021-01-31 20:05:30 +0800

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -6,6 +6,3 @@ update(){
 upgrade(){
   yum update-minimal # as a minimal upgrade for neccessary
 }
-update2latest(){
-  yum install https://repo.ius.io/ius-release-el7.rpm  https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-}
