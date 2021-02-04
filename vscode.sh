4178a3da4762fa1182c99ea860106dbaf852d49a
David Liu <david.yx.liu@oracle.com>
2021-02-03 23:14:55 -0500

    Create vscode.sh

---
+++ vscode.sh
@@ -0,0 +1,9 @@
+
+## MS currently ship the stable 64-bit VS Code in a yum repository
+install(){
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  yum check-update
+  sudo yum install code
+}
+$@
