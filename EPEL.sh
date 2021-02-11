4e610db99eb8fa820244cb353ae43ceeeb4bb986
David Liu <david.yx.liu@oracle.com>
2021-02-10 22:37:37 -0500

    Create EPEL.sh

---
+++ EPEL.sh
@@ -0,0 +1,18 @@
+Redhat8(){
+  sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+  sudo dnf upgrade
+}
+
+Redhat7(){
+  sudo rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+}
+optional(){
+  sudo subscription-manager repos --enable "rhel-*-optional-rpms"
+  sudo yum update
+}
+extra(){
+  sudo subscription-manager repos --enable "rhel-*-extras-rpms"
+  sudo yum update
+}
+$@
