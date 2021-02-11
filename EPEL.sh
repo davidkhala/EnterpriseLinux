3e519775cafd51f05b0644eb81f5adcc22cad736
David Liu <david.yx.liu@oracle.com>
2021-02-10 22:50:32 -0500

    Update EPEL.sh

--- EPEL.sh
+++ EPEL.sh
@@ -1,12 +1,7 @@
 # Extra Packages for Enterprise Linux (EPEL) 
-Redhat8(){
-  sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-  sudo dnf upgrade
-}
-
-Redhat7(){
-  sudo rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
+install(){
+  sudo yum install epel-release
+  
 }
 
 $@
