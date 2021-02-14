b221a5f628578d96b8eb17cc5669f17afbeb0c71
DavidLiu <david.yx.liu@oracle.com>
2021-02-13 20:40:14 -0500

    add OCR login

--- oracle-linux/docker.sh
+++ oracle-linux/docker.sh
@@ -2,4 +2,8 @@ install(){
     sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
     sudo dnf install -y docker-ce --nobest --allowerasing
 }
+OCR(){
+    # Switch to Oracle Container Registry
+    docker login container-registry.oracle.com
+}
 $@
