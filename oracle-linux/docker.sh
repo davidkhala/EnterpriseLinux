71fb7d7608fa453d749af4d91dbbc619ecc5cee4
DavidLiu <david.yx.liu@oracle.com>
2021-02-14 22:00:25 -0500

    split into oracle-linux-utils

--- oracle-linux/docker.sh
+++
@@ -1,9 +0,0 @@
-install(){
-    sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-    sudo dnf install -y docker-ce --nobest --allowerasing
-}
-OCR(){
-    # Switch to Oracle Container Registry
-    docker login container-registry.oracle.com
-}
-$@
