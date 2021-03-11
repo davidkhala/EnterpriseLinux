b5b02ffecfbfd34e047b249889ee9e3052cc77bd
DavidLiu <david.yx.liu@oracle.com>
2021-03-10 23:35:15 -0500

    add smartGit installer

--- smartGit.sh
+++ smartGit.sh
@@ -1,9 +1,13 @@
+set -e
 install(){
-  wget https://www.syntevo.com/downloads/smartgit/smartgit-linux-20_2_4.tar.gz
+  wget https://www.syntevo.com/downloads/smartgit/smartgit-linux-20_2_4.tar.gz -O $HOME/smartgit-linux-20_2_4.tar.gz
+  cd $HOME
   tar xzf smartgit*.tar.gz
-  bin/add-menuitem.sh
-
+  smartgit/bin/add-menuitem.sh
+  rm smartgit*.tar.gz
+  cd -
 }
+# TODO
 remove(){
   bin/remove-menuitem.sh
   # then `rm ...`
