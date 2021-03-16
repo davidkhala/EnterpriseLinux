51f76ae72f86daca0d454758dd03a55916d05098
David Liu <david.yx.liu@oracle.com>
2021-03-16 22:57:43 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,13 +1,17 @@
 # man: https://firewalld.org/documentation/man-pages/firewall-cmd.html
+set -e
 addPort(){
     # Error: INVALID_PORT: bad port (most likely missing protocol), correct syntax is portid[-portid]/protocol
-    sudo firewall-cmd --add-port=$1
+    sudo firewall-cmd --permanent --add-port=$1
+    sudo firewall-cmd --reload
+    
 }
 listPorts(){
     sudo firewall-cmd --list-ports
 }
 removePort(){
     sudo firewall-cmd --remove-port=$1
+    sudo firewall-cmd --reload
 }
 
 $@
