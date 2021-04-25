ee565fc92c5434e8a15fa89a96f81e7245d66fdf
DavidLiu <david.yx.liu@oracle.com>
2021-04-25 10:22:06 -0400

    [done] test python.sh install2

--- python.sh
+++ python.sh
@@ -1,8 +1,8 @@
 set -e
 install2(){
   sudo yum install python2
-  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  python get-pip.py
+  curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+  python2 get-pip.py
   rm get-pip.py
 
 }
