821469a32917d6bb9fe2d629c6acb1c41b344691
David Liu <david.yx.liu@oracle.com>
2021-05-09 11:17:44 -0400

    Create gradle.sh

---
+++ java/gradle.sh
@@ -0,0 +1,13 @@
+# https://gradle.org/install/ said: 
+#   SDKMAN! is a tool for managing parallel versions of multiple Software Development Kits on most Unix-based systems.
+#   https://github.com/sdkman
+set -e
+SDKMAN_install(){
+  curl -s "https://get.sdkman.io" | bash
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+  sdk version
+}
+install(){
+  SDKMAN_install
+  sdk install gradle 7.0
+}
