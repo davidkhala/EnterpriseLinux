8697b4d8c00785acfaa07e43f874bf8c735e1794
David Liu <david.yx.liu@oracle.com>
2021-07-30 06:04:41 -0400

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 install() {
-    curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
+    curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
     sudo yum install -y nodejs
 }
 nodeGYPDependencies() {
