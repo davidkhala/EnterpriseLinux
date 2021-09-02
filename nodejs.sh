db6af237601cd7782b5722e62ed22de02582e52c
David Liu <david.yx.liu@oracle.com>
2021-09-01 21:51:26 -0400

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 install() {
-    curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-    sudo yum install -y nodejs
+    sudo dnf install @nodejs:14
 }
 nodeGYPDependencies() {
     sudo yum install -y gcc-c++
