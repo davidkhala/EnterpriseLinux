70bc9f16332c7579c52a0baee30ed0320319d5af
David Liu <david.yx.liu@oracle.com>
2021-09-24 18:15:22 +0800

    Delete docker.sh

--- docker.sh
+++
@@ -1,9 +0,0 @@
-install(){
-  curl -sSL https://get.docker.com/ | sh
-  
-  sudo systemctl enable docker.service
-  sudo systemctl enable containerd.service
-  sudo usermod -aG docker $USER
-  # log in again
-}
-$@
