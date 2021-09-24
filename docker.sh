cea4eb1634c6bb03f3879f886a5c62286a16c2ba
David Liu <david.yx.liu@oracle.com>
2021-09-23 21:23:27 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,8 @@
 install(){
   curl -sSL https://get.docker.com/ | sh
-  sudo systemctl start docker
+  
+  sudo systemctl enable docker.service
+ 	sudo systemctl enable containerd.service
   sudo usermod -aG docker $USER
   # log in again
 }
