3248bd73f2e2c35729d6cd6489944fca660a89af
David Liu <david.yx.liu@oracle.com>
2021-10-07 04:42:09 -0400

    Update cli.sh

--- gcp/cli.sh
+++ gcp/cli.sh
@@ -1,3 +1,4 @@
+set -e
 ## The gcloud tool is a part of the Cloud SDK. You must download and install the Cloud SDK on your system and initialize it before you can use the gcloud tool.
 setup(){
   sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
