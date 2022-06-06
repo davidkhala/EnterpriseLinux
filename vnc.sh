da9a6759e5d63cd97bf0a4e57a447817a6d8bedc
David Liu <david.yx.liu@oracle.com>
2022-06-06 11:31:21 +0800

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -1,4 +1,10 @@
 install-server(){
   sudo yum install -y tigervnc-server
 }
+rdp(){
+  sudo yum install -y xrdp
+  sudo systemctl enable --now xrdp.service
+  sudo firewall-cmd --permanent --add-port=3389/tcp 
+  sudo firewall-cmd --reload
+}
 $@
