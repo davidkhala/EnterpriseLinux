0f64a6b7e070a8cbef6b4728eb4fb51df0f970b7
David Liu <david.yx.liu@oracle.com>
2022-07-14 01:47:49 -0400

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,10 +1,23 @@
-install(){
+set -e
+install-fedora36(){
   sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/36/winehq.repo
   sudo dnf install winehq-stable
  
 }
-remove(){
+remove-fedora36(){
+  sudo dnf remove winehq-stable
   rm /etc/yum.repos.d/winehq.repo
+}
+build(){
+  cd /opt
+  wget https://dl.winehq.org/wine/source/5.x/wine-5.1.tar.xz
+  tar -Jxf wine-5.1.tar.xz
+  rm wine-5.1.tar.xz
+  cd wine-5.1
+  ./configure  --enable-win64
+  make
+  make install
+  wine64 --version 
 }
 
 $@
