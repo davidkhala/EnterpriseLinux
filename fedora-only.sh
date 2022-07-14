b32b671c6eedecd551dbd22832614c2f8a0dc9b8
David Liu <david.yx.liu@oracle.com>
2022-07-14 01:32:47 -0400

    Update fedora-only.sh

--- fedora-only.sh
+++ fedora-only.sh
@@ -2,7 +2,7 @@ set -e
 system-upgrade(){
   sudo dnf upgrade --refresh
   sudo dnf install dnf-plugin-system-upgrade
-  sudo dnf system-upgrade -y download --releasever 36
+  sudo dnf system-upgrade -y download --releasever rawhide
   sudo dnf system-upgrade reboot
 }
 $@
