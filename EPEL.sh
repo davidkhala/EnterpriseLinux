e97024387bc8fcef737e339a2ba639124c30611f
David Liu <david.yx.liu@oracle.com>
2022-10-20 16:37:29 +0000

    [TODO] clean up structure

--- EPEL.sh
+++
@@ -1,7 +0,0 @@
-# Extra Packages for Enterprise Linux (EPEL) 
-install(){
-  sudo yum install epel-release
-  
-}
-
-$@
--- system/install.sh
+++ system/install.sh
@@ -1,1 +1,10 @@
+pinyin() {
+    sudo yum install -y ibus-libpinyin
+    reboot
+}
+# Extra Packages for Enterprise Linux (EPEL)
+EPEL() {
+    sudo yum install -y epel-release
+}
 
+$@
