a49791b94447b28aee3e8196e22ef0d8ef2049bd
David Liu <david.yx.liu@oracle.com>
2022-10-22 22:24:58 +0800

    Update minio.sh

--- minio.sh
+++ minio.sh
@@ -1,13 +1,20 @@
+user=${2:admin}
+password=${3:password}
+path=${4:/mnt/data}
 install-server(){
   sudo dnf install -y https://dl.min.io/server/minio/release/linux-amd64/minio-20221021223748.0.0.x86_64.rpm
 }
-start-server(){
-  local user=${1:admin}
-  local password=${2:password}
-  local path=${3:/mnt/data}
+start-attach(){
   sudo mkdir $path
   sudo chown -R $USER $path && sudo chmod u+rxw $path
   MINIO_ROOT_USER=$user MINIO_ROOT_PASSWORD=$password minio server $path --console-address ":9001"
 
 }
+start-docker(){
+  sudo docker run -d  \
+     -p 9000:9000 -p 9001:9001 --volume $path:/data
+     -e "MINIO_ROOT_USER=${user}" -e "MINIO_ROOT_PASSWORD=${password}" \
+     quay.io/minio/minio server /data --console-address ":9001"
+}
+
 $@
