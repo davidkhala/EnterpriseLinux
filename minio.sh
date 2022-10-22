04c48dcb063b9d3a4ac1137d6fc935414b3858a6
David Liu <david.yx.liu@oracle.com>
2022-10-22 21:42:36 +0800

    Update minio.sh

--- minio.sh
+++ minio.sh
@@ -2,7 +2,10 @@ install-server(){
   sudo dnf install -y https://dl.min.io/server/minio/release/linux-amd64/minio-20221021223748.0.0.x86_64.rpm
 }
 start-server(){
-  MINIO_ROOT_USER=admin MINIO_ROOT_PASSWORD=password minio server /mnt/data --console-address ":9001"
+  local path=${1:/mnt/data}
+  sudo mkdir $path
+  sudo chown -R $USER $path && sudo chmod u+rxw $path
+  MINIO_ROOT_USER=admin MINIO_ROOT_PASSWORD=password minio server $path --console-address ":9001"
 
 }
 $@
