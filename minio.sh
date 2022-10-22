31b4e1653f2d20db566156460c6763f6d8d73839
David Liu <david.yx.liu@oracle.com>
2022-10-22 23:04:48 +0800

    Update minio.sh

--- minio.sh
+++ minio.sh
@@ -11,7 +11,9 @@ start-attach(){
 
 }
 start-docker(){
-  docker run -d -p 9000:9000 -p 9001:9001 -e "MINIO_ROOT_USER=${user}" -e "MINIO_ROOT_PASSWORD=${password}" quay.io/minio/minio server /data --console-address ":9001"
+  docker run -d -p 9000:9000 -p 9001:9001 -e "MINIO_ROOT_USER=${user}" -e "MINIO_ROOT_PASSWORD=${password}" \
+    -v $path:/data \
+    quay.io/minio/minio server /data --console-address ":9001"
 }
 
 $@
