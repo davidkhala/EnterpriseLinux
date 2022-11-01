535c0405a42ee5309b801120b098d78cbb48657e
David Liu <david.yx.liu@oracle.com>
2022-11-01 10:01:21 +0000

    firewalld not found in fedora

--- firewall.sh
+++ firewall.sh
@@ -1,18 +1,18 @@
 # man: https://firewalld.org/documentation/man-pages/firewall-cmd.html
 set -e
-addPort(){
+
+addPort() {
     # Error: INVALID_PORT: bad port (most likely missing protocol), correct syntax is portid[-portid]/protocol
     sudo firewall-cmd --permanent --add-port=$1
     sudo firewall-cmd --reload
-    
+
 }
-listPorts(){
+listPorts() {
     sudo firewall-cmd --list-ports
 }
-removePort(){
+removePort() {
     sudo firewall-cmd --remove-port=$1
     sudo firewall-cmd --reload
 }
 
 $@
-
--- system/fedora-only.sh
+++ system/fedora-only.sh
@@ -1,16 +1,19 @@
 set -e
-system-upgrade(){
+system-upgrade() {
   sudo dnf upgrade --refresh
   sudo dnf install dnf-plugin-system-upgrade
   sudo dnf system-upgrade -y download --releasever rawhide
   sudo dnf system-upgrade reboot
 }
-system-upgrade-post(){
+system-upgrade-post() {
   sudo dnf install rpmconf remove-retired-packages
   sudo rpmconf -a
   remove-retired-packages
   sudo dnf update
   sudo dnf autoremove
 
+}
+common-utils() {
+  sudo dnf install -y firewalld
 }
 $@
