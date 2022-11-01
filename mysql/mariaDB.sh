798566e03a47dbf497db7b4f5b79bb397f975a7c
David Liu <david.yx.liu@oracle.com>
2022-11-01 11:59:30 +0000

    add post install wizard

--- mysql/mariaDB.sh
+++ mysql/mariaDB.sh
@@ -1,6 +1,10 @@
-install(){
+set -e
+install() {
+  # According to https://docs.rackspace.com/support/how-to/installing-mariadb-server-on-centos/
   sudo dnf install -y mariadb-server
   sudo firewall-cmd --add-service=mysql --permanent
   sudo systemctl enable --now mariadb.service
+  /usr/bin/mysql_secure_installation
+
 }
 $@
