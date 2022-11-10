4a2797a524e35bb949e94b190b4ae3ec5b7e0bed
David Liu <david.yx.liu@oracle.com>
2022-11-10 16:26:57 +0800

    Update mariaDB.sh

--- mysql/mariaDB.sh
+++ mysql/mariaDB.sh
@@ -3,8 +3,8 @@ install() {
   # According to https://docs.rackspace.com/support/how-to/installing-mariadb-server-on-centos/
   sudo dnf install -y mariadb-server
   curl https://raw.githubusercontent.com/davidkhala/fedora-utils/master/mysql/setup.sh | bash -s firewall
-  sudo systemctl enable --now mariadb.service
   /usr/bin/mysql_secure_installation
+  sudo systemctl enable --now mariadb.service
 
 }
 $@
