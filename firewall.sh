9e2194f02ac802f36a276c48737c8945280d0607
David Liu <david.yx.liu@oracle.com>
2022-11-13 02:46:01 +0000

    use - than Camel

--- firewall.sh
+++ firewall.sh
@@ -1,18 +1,24 @@
 # man: https://firewalld.org/documentation/man-pages/firewall-cmd.html
 set -e
 
-addPort() {
+add-port() {
     # Error: INVALID_PORT: bad port (most likely missing protocol), correct syntax is portid[-portid]/protocol
     sudo firewall-cmd --permanent --add-port=$1
-    sudo firewall-cmd --reload
-
+    reload
 }
-listPorts() {
+add-service() {
+    sudo firewall-cmd --permanent --add-service=$1
+    reload
+}
+reload() {
+    sudo firewall-cmd --reload
+}
+list-port() {
     sudo firewall-cmd --list-ports
 }
-removePort() {
+remove-port() {
     sudo firewall-cmd --remove-port=$1
-    sudo firewall-cmd --reload
+    reload
 }
 
 $@
