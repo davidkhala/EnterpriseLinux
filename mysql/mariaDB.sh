2c3342bcb59e51bc12961182684542bfcce38ddb
David Liu <david.yx.liu@oracle.com>
2022-11-13 06:36:07 +0000

    cross project integrate

--- mysql/mariaDB.sh
+++ mysql/mariaDB.sh
@@ -2,10 +2,7 @@ set -e
 install() {
   # According to https://docs.rackspace.com/support/how-to/installing-mariadb-server-on-centos/
   sudo dnf install -y mariadb-server
-  curl https://raw.githubusercontent.com/davidkhala/fedora-utils/master/mysql/setup.sh | bash -s firewall
-  /usr/bin/mysql_secure_installation
-  sudo systemctl enable --now mariadb.service
+  curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/database/mariaDB.sh | bash -s post-install
 
 }
 $@
-  
