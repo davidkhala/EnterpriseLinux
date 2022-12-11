d5b773b205bb6d73d8a8113bff9c6fd0e3bd5a0b
David Liu <david.yx.liu@oracle.com>
2022-12-11 20:01:02 +0800

    Update and rename vnc.sh to connect.sh

--- connect.sh
+++ connect.sh
@@ -1,7 +1,9 @@
-install-server() {
+install-vnc() {
   sudo yum install -y tigervnc-server
 }
 rdp() {
+  install-vnc
+  # xrdp package is available on EPEL repository. Prepare it in advance.
   sudo yum install -y xrdp
   sudo systemctl enable --now xrdp.service
   sudo firewall-cmd --permanent --add-port=3389/tcp
