6aa62911393f0bd0578d6b6a0dfe9009f03c4728
David Liu <david.yx.liu@oracle.com>
2022-12-17 08:54:26 +0800

    Update nodejs.sh

--- language/nodejs.sh
+++ language/nodejs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 install() {
-    sudo dnf install -y @nodejs:14
+#   switch enabled streams of a module
+    sudo dnf module reset -y nodejs
+    sudo dnf install -y @nodejs:18
 }
 nodeGYPDependencies() {
     sudo yum groupinstall -y "Development Tools"
