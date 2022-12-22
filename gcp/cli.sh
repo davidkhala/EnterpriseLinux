9a086d0f30cba65eacedf15f376bbb1ba1daae0c
David Liu <david-khala@hotmail.com>
2022-12-22 15:48:12 +0800

    trim tips

--- gcp/cli.sh
+++ gcp/cli.sh
@@ -1,6 +1,5 @@
 set -e
-## The gcloud tool is a part of the Cloud SDK. You must download and install the Cloud SDK on your system and initialize it before you can use the gcloud tool.
-
+## initialize it before you can use the gcloud tool.
 install() {
   curl https://raw.githubusercontent.com/davidkhala/centos-collection/main/gcp/cli.sh | bash -s install
   
